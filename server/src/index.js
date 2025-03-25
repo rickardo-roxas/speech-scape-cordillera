1b642be8963b8340169bf0959245957a99af993d
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-25 22:13:41 +0800

    Added initial routes

--- server/src/index.js
+++ server/src/index.js
@@ -3,16 +3,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const landingRoutes = require('./routes/LandingRoutes');
+const mapRoutes = require('./routes/MapRoutes');
+const provinceRoutes = require('./routes/ProvinceRoutes');
+const searchRoutes = require('./routes/SearchRoutes'); 
 
-app.get("/", (req, res => {
-    res.setEncoding("Connected to Server.");
-}))
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+/* Routes */
+app.use("/api/", landingRoutes);
+app.use("/api/map", mapRoutes);
+app.use("/api/province", provinceRoutes);
+app.use("/api/search", searchRoutes);
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
