7f46ad9b39459f9610c14272e870d663cbf4dd04
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-01 02:58:57 +0800

    Fixed api route

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
     res.send("Connected to Server.");
 });
 
