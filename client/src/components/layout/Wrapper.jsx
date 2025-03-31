702928637751672899868ca97ea801aa13a91e09
Johan Rickardo A. Roxas <75343741+rickardo-roxas@users.noreply.github.com>
2025-04-01 01:13:36 +0800

    Update Wrapper.jsx

--- client/src/components/layout/Wrapper.jsx
+++ client/src/components/layout/Wrapper.jsx
@@ -1,14 +1,14 @@
-// import Navbar from "./Navbar";
-// import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-// function LayoutWrapper({ children }) {
-//   return (
-//     <>
-//       <Navbar />
-//       <main>{children}</main>
-//       <Footer />
-//     </>
-//   );
-// }
+function Wrapper({ children }) {
+   return (
+     <>
+       <Navbar />
+       <main>{children}</main>
+       <Footer />
+     </>
+   );
+}
 
-// export default LayoutWrapper;
+export default Wrapper;
