998dfef3072fd603b855ea91143aab48c8805e52
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-31 16:58:14 +0800

    Added url import

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
 
