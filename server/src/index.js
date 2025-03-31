a7dc1ad7fa2c0d2ae55ce0cee82194dcb709bf89
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-31 16:52:00 +0800

    Fixed path import

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { dirname } from "path";
+import path from "path";
 
 const app = express();
 
@@ -11,5 +11,3 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
-
-console.log(__dirname);
