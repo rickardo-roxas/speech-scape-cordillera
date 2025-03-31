e4437636f98851ad8b5238efeca32b9ec992e8ce
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-01 01:26:55 +0800

    Added test scripts

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
