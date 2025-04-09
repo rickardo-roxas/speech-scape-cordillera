5c4bd39f6083f1d16be2f9784b9d215afbe83472
saintramon <ramonjasmin8@gmail.com>
2025-04-09 16:40:45 +0800

    Initial language model

--- server/src/models/Language.model.js
+++ server/src/models/Language.model.js
@@ -5,3 +5,42 @@ class Language{
         this.name = name;
     }
 }
+
+/**
+ * Returns all available languages
+ */
+async function getAllLanguages() {
+    try {
+        const languages = await databaseUtils.getAll('languages');
+
+        return languages; 
+
+    } catch (err) {
+        console.error("Failed to fetch all languages", err);
+        throw err;
+    }
+}
+
+/**
+ * Returns a language based by language_id
+ */
+async function getLanguageByID(id) {
+    try {
+        const query = "SELECT * FROM languages WHERE language_id = ?";
+        const language = await databaseUtils.performQuery(query, [id]);
+
+        if (language.length === 0) {
+            throw new Error("Language not found");
+        }
+
+        return language[0]; 
+    } catch (err) {
+        console.error("Failed to fetch language with id " + id, err);
+        throw err; 
+    }
+}
+
+export default{
+    getAllLanguages,
+    getLanguageByID
+};
