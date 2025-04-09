8108259cd7dd400a4191e9a23e996b3e48b793fc
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-09 13:36:58 +0800

    Fixed environment variables

--- client/src/services/API.service.js
+++ client/src/services/API.service.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+/**
+ * Establishes a connection to the API using Axios.
+ */
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
