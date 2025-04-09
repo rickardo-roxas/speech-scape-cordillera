4f94fec16a4f67b9189527d875b70f339275dc55
saintramon <ramonjasmin8@gmail.com>
2025-04-09 11:56:46 +0800

    Added municipality class

--- server/src/models/Municipalities.model.js
+++ server/src/models/Municipalities.model.js
@@ -1,1 +1,8 @@
 import databaseUtils from '../utils/Database.utils.js';
+
+class Municipality{
+    constructor(name, description){
+        this.name = name;
+        this.description = description;
+    }
+}
