96c17d362b46e02fa40f4653f897b57e220617f7
saintramon <ramonjasmin8@gmail.com>
2025-04-09 12:08:49 +0800

    initial Province class

--- server/src/models/Province.model.js
+++ server/src/models/Province.model.js
@@ -1,1 +1,12 @@
 import databaseUtils from '../utils/Database.utils.js';
+
+class Province{
+        constructor(name, information, municipalities, ethnicities, languages, images){
+        this.name = name;
+        this.information = information;
+        this.municipalities = municipalities;
+        this.ethnicities = ethnicities;
+        this.languages = languages;
+        this.images = images;
+    }
+}
