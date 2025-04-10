d41f1fcdc9998b8e1620d9f7e42579f1b6507db5
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-11 00:12:23 +0800

    Removed redundant functions

--- server/src/models/Municipalities.model.js
+++ server/src/models/Municipalities.model.js
@@ -1,11 +1,14 @@
 import databaseUtils from '../utils/Database.utils.js';
 
+/**
+ * Class representing a municipality.
+ * Languages may be null or undefined.
+ */
 class Municipality {
-    constructor(municipality_id, m_name, m_information, province_id) {
-        this.municipality_id = municipality_id;
+    constructor(m_name, m_information, languages) {
         this.m_name = m_name;
         this.m_information = m_information;
-        this.province_id = province_id;
+        this.languages = languages;
     }
 }
 
@@ -16,38 +19,13 @@ class Municipality {
 async function getAllMunicipalities() {
     try {
         const municipalities = await databaseUtils.getAll('municipalities');
+
         return municipalities.map((municipality) => new Municipality(
-            municipality.municipality_id,
             municipality.m_name,
             municipality.m_information,
-            municipality.province_id
         ));
     } catch (err) {
         console.error('Error fetching all municipalities:', err);
-        throw err;
-    }
-}
-
-/**
- * Retrieves a municipality through its province ID.
- * @param {integer} id - The ID of the province to retrieve municipalities from.
- * @returns {Promise} - Resolves with the results of the query or rejects with error.
- */
-async function getMunicipalitiesOfProvince(id) {
-    try {
-        const municipalities = await databaseUtils.performQuery(
-            'SELECT * FROM municipalities WHERE province_id = ?',
-            [id]
-        );
-
-        return municipalities.map((municipality) => new Municipality(
-            municipality.municipality_id,
-            municipality.m_name,
-            municipality.m_information,
-            municipality.province_id
-        ));
-    } catch (err) {
-        console.error('Error fetching municipalities of province:', err);
         throw err;
     }
 }
