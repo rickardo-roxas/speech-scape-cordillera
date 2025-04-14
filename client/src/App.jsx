694a6a06ee5b236a40794c236bf69dff34998505
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-14 13:05:49 +0800

    Fixed bootstrap dependency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from "react-hot-toast";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Pages and components
 import Wrapper from './components/layout/Wrapper';
--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,8 +1,14 @@
+// Libraries and dependencies
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/Styles.css";
+
 import App from './App.jsx';
 
+/**
+ * Main entry point for the React application.
+ */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
