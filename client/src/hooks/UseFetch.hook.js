46d0c769e40868381e808b0ca32b9721b1053a1d
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-14 12:37:16 +0800

    Added get and post handling function and toaster intercepts

--- client/src/hooks/UseFetch.hook.js
+++ client/src/hooks/UseFetch.hook.js
@@ -1,24 +1,44 @@
 import { useState, useEffect } from "react";
-import axios from "../services/apiService"; // Uses Axios service
+import api from "../configs/API.config";
 
-export default function useFetch(url) {
-    const [data, setData] = useState(null);
+/**
+ * Fetch data from an API endpoint using Axios.
+ * @param {url} url - The API endpoint to fetch data from.
+ * @param {method,data,config} param1 - An object containing the method, data, and config for the request.
+ * @returns {Object} - An object containing the response data, loading state, error state, and a refetch function.
+ */
+export default function useFetch(url, { method = "get", data = null, config = {} } = {}) {
+    const [responseData, setResponseData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(url);
-                setData(response.data);
-            } catch (err) {
-                setError(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, [url]);
+    /**
+     * Fetch data from the API.
+     */
+    const fetchData = async () => {
+        setLoading(true); 
+        try {
+            const response = await api.request({
+                url,
+                method, // GET and POST
+                data, // for POST
+                ...config, // headers, params, etc.
+            });
+            setResponseData(response.data); // response.data is the data returned from the API
+            setError(null); // clear any previous errors
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    return { data, loading, error };
+    /**
+     * Fetch data when the component mounts or when dependencies change.
+     */
+    useEffect(() => {
+        fetchData();
+    }, [url, method, JSON.stringify(data), JSON.stringify(config)]);
+
+    return { data: responseData, loading, error, refetch: fetchData };
 }
