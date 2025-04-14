20c63690e8c36ae0310a95b2a63d00b3ebba8100
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-14 12:37:33 +0800

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,30 @@
+// Libraries and dependencies
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Pages and components
 import Wrapper from './components/layout/Wrapper';
-// Import pages here.
+import LandingPage from './pages/LandingPage';
+import ProvinceAndCities from './pages/ProvinceAndCities';
+import ProvinceCity from './pages/ProvinceCity';
+import Map from './pages/Map';
 
+/**
+ * Main application component.
+ * @returns {JSX.Element} - The main application component.
+ */
 function App() {
   return (
     <Router>
+      <Toaster position='top-center' reverseOrder={false} />
       <Wrapper>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          // To define other routes
+          <Route path="/" element={ LandingPage } />
+          <Route path="/map" element={ Map } />
+          <Route path="/provinces" element={ ProvinceAndCities } />
+          <Route path="/provinces/:name" element={ ProvinceCity } />
         </Routes>
       </Wrapper>
     </Router>
