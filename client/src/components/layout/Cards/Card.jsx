c1a4d3fb6e160fb3ba1136880449d8df5be03518
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-15 17:16:33 +0800

    Fixed header radius and added drop shadow

--- client/src/components/layout/Cards/Card.jsx
+++ client/src/components/layout/Cards/Card.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Card as BootstrapCard } from 'react-bootstrap';
 
-function Card() {
+import styles from './Card.module.css';
+
+/**
+ * Reusable Card component that renders a card with an optional header.
+ * @param {object} param0 - Component props
+ * @param {string} param0.header - Optional card header
+ * @param {string} param0.children - Card body content. Can contain multiple elements.
+ * @returns {JSX.Element} - Rendered card component
+ */
+function Card({ header, children, className = '' }) {
   return (
-    <div>Card</div>
+    <BootstrapCard className={`${styles.card} ${className}`}>
+      {header && (
+        <BootstrapCard.Header className={styles.header}>
+          <h2>
+            {header}
+          </h2>
+        </BootstrapCard.Header>
+      )}
+      <BootstrapCard.Body className={styles.body}>
+        {children}
+      </BootstrapCard.Body>
+    </BootstrapCard>
   );
 }
 
