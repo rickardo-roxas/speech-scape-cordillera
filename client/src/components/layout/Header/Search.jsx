002d0583722f5db75e6356df383a28e7ffd4e731
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-15 14:06:07 +0800

    Finalized search style

--- client/src/components/layout/Header/Search.jsx
+++ client/src/components/layout/Header/Search.jsx
@@ -1,29 +1,33 @@
+// Libraries
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+// Local
 import Image from '../../ui/Image';
+import icon from '../../../assets/icons/search_solid.png';
+
 import styles from './Search.module.css';
 
 /**
  * Reusable Search component that renders a search input field and suggestions.
  * @returns {JSX.Element} - Rendered Search component
  */
-function Search({ className = "" }) {
+function Search() {
     return (
-        <Form className={`${styles['search']} ${className}`}>
-            <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">
+        <Form className={styles.search}>
+            <InputGroup className="">
+                <InputGroup.Text id="basic-addon1" className={styles.icon}>
                     <Image
-                        src="../../../public/icons/search_solid.png"
+                        src={icon}
                         alt="Search Icon"
                         width={20}
                         height={20}
                     />
                 </InputGroup.Text>
                 <Form.Control
-                    placeholder="Search"
+                    placeholder="Search province or city..."
                     aria-label="Search"
                     aria-describedby="basic-addon1"
                 />
