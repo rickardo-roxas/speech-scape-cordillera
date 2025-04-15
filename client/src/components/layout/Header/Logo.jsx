fbc0e558b165dcfa0197723b0f83b40c85b53c58
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-15 15:01:24 +0800

    Added documentation

--- client/src/components/layout/Header/Logo.jsx
+++ client/src/components/layout/Header/Logo.jsx
@@ -8,6 +8,10 @@ import logo from '../../../assets/icons/logo_solid.png';
 
 import styles from './Logo.module.css';
 
+/**
+ * Reusable Logo component that renders the logo and title of SpeechScape.
+ * @returns {JSX.Element} - Rendered Logo component
+ */
 function Logo() {
     return (
         <Link to="/" className={styles.logo}>
