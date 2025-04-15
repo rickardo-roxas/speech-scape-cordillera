d6adbbf908632d2ba8cc619338564e59fc270dc1
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-15 11:26:25 +0800

    Initial scaffold for layout

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../common/Logo';
+import Search from '../forms/Search';
+import LinkButton from '../common/button/LinkButton';
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+      <Logo />
+      <Search />
+      <LinkButton />
+    </>
   );
 }
 
--- client/src/components/layout/Wrapper.jsx
+++ client/src/components/layout/Wrapper.jsx
@@ -1,14 +1,17 @@
 import Navbar from "./Navbar";
-import Footer from "./Footer";
 
+/**
+ * Wrapper component that serves as a layout for the application.
+ * @param {children} param0 - The children elements to be rendered inside the wrapper.
+ * @returns {JSX.Element} - The wrapper component with a navbar, main content, and footer.
+ */
 function Wrapper({ children }) {
-   return (
-     <>
-       <Navbar />
-       <main>{children}</main>
-       <Footer />
-     </>
-   );
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
 }
 
 export default Wrapper;
