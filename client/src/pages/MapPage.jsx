5779d1c4e7630a40e3060b0696b0536fb3829a74
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-17 13:53:50 +0800

    Added documentation and placeholder for state management Megred and resolved conflicts

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,13 +1,12 @@
-// Libraries and dependencies
-import React from 'react';
+import React, { useState } from 'react';
 // import useFetch from '../hooks/UseFetch.hook';
 
-// Components
 import Card from '../components/layout/Cards/Card';
 import TextContainer from '../components/layout/TextContainer/TextContainer';
 import Badge from '../components/ui/Badge/Badge';
 import PhilippineMap from '../components/features/map/PhilippineMap';
 
+import cordilleraGeoJSON from '../assets/json/cordillera.json';
 import styles from './MapPage.module.css';
 
 /**
@@ -15,6 +14,21 @@ import styles from './MapPage.module.css';
  * @returns {JSX.Element} 
  */
 function MapPage() {
+    //const [selectedProvince, setSelectedProvince] = useState(null);
+    //const [provinceData, setProvinceData] = useState(null);
+
+    /**
+     * Function to handle events for each GeoJSON feature upon user click.
+     * @param {object} feature - GeoJSON feature
+     * @param {L.layer} layer - Leaflet layer 
+     */
+    const onEachFeature = (feature, layer) => {
+        layer.bindPopup(feature.properties.name);
+        layer.on('click', () => {
+            setSelectedProvince(feature.properties.name);
+        });
+    }
+
     return (
         <div className={styles.div}>
             <Card
@@ -45,7 +59,13 @@ function MapPage() {
                 header="Cordillera Administrative Region"
                 className={styles.map}
             >
-                <PhilippineMap />
+                <PhilippineMap
+                    scrollWheelZoom={true}
+                    dragging={true}
+                    zoomControl={true}
+                    onEachFeature={onEachFeature}
+                    geojson={cordilleraGeoJSON}
+                />
             </Card> 
         </div>
     );
