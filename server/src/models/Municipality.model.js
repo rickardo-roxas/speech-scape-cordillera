7cf1b4ddd8f3880d1fd2512acfa9bf3fdaa2a457
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-17 15:51:46 +0800

    Simplified municipality model

--- server/src/models/Municipality.model.js
+++ server/src/models/Municipality.model.js
@@ -5,9 +5,15 @@ import databaseUtils from '../utils/Database.utils.js';
  * Languages may be null or undefined.
  */
 class Municipality {
-    constructor(m_name, m_information, languages) {
-        this.m_name = m_name;
-        this.m_information = m_information;
+    /**
+     * Creates a Municipality object.
+     * @param {String} municipality_name - Name of municipality.
+     * @param {String} info - Information bullet on the municipality.
+     * @param {array} languages - Array of language objects.
+     */
+    constructor(municipality_name, info, languages) {
+        this.municipality_name = municipality_name;
+        this.info = info;
         this.languages = languages;
     }
 }
@@ -30,36 +36,7 @@ async function getAllMunicipalities() {
     }
 }
 
-/**
- * Retrieves all municipalities for a specific Province
- * @param {*} id 
- * @returns 
- */
-async function getMunicipalitiesByProvinceID(id){
-    try{
-
-        const query = "SELECT m_name, m_information FROM municipalities WHERE province_id = ?";
-
-        const municipalities = await databaseUtils.performQuery(query, [id]);
-
-        if(municipalities.length === 0){
-            console.log("No municipalities found for province ID " + id);
-            return [];
-        }
-
-        return municipalities.map(municipality => new Municipality(
-            municipality.m_name,
-            municipality.m_information,
-            null
-        ));
-
-    }catch(err){
-        console.error("Failed to fetch municipalities of province ID " + id);
-        throw err;
-    }
-}
-
 export default {
-    getAllMunicipalities,
-    getMunicipalitiesByProvinceID
+    Municipality,
+    getAllMunicipalities
 };
