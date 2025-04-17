22b70f30d5cada5e1e29153c8f754c8e9e8a0cf4
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-17 14:45:51 +0800

    Added barangay model

---
+++ server/src/models/Barangay.model.js
@@ -0,0 +1,31 @@
+import DatabaseUtils from "../utils/Database.utils";
+
+/**
+ * Class representing a barangay.
+ */
+class Barangay {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+/**
+ * Returns all barangays from the database.
+ * @returns {Promise} - Resolves with the results of the query or rejects with error.
+ */
+async function getAllBarangays() {
+    try {
+        const barangays = await DatabaseUtils.getAll('city_barangays');
+        return barangays.map((barangay) => new Barangay(
+            barangays.barangay_name
+        ))
+    } catch(err) {
+        console.error("Failed to fetch all cities");
+        throw err;
+    }
+}
+
+export default {
+    Barangay,
+    getAllBarangays
+}
