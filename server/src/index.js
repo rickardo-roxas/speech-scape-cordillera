600c3fc502246e1843560c1cbaf5f0e1187c0544
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-17 18:27:30 +0800

    Centralized handling of  environment variables

--- server/src/index.js
+++ server/src/index.js
@@ -3,22 +3,21 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-import dotenv from "dotenv";
 
 // Middlewares
 import corsOptions from "./configs/Cors.config.js";
 import errorHandler from "./middlewares/ErrorHandler.middleware.js";
 import logger from "./middlewares/Logger.middleware.js";
 
+// Environment Variables
+import config from './configs/Index.config.js';
+
 // Routes
 import searchRoutes from "./routes/Search.routes.js";
 import mapRoutes from "./routes/Map.routes.js";
 import provinceRoutes from "./routes/Province.routes.js";
 
-dotenv.config();
-
 // Environment Variables
-const PORT = process.env.PORT;
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
@@ -31,16 +30,8 @@ app.use(express.static(path.join(__dirname, "../public",)));
 app.use(logger);
 
 // Routes
-
-// For testing purposes
-app.get("/", (req, res, next) => {
+app.get("/api/", (req, res) => { 
     res.send("Connected to Server.");
-    next();
-});
-
-app.get("/api/", (req, res, next) => { 
-    res.send("Connected to Server.");
-    next();
 });
 app.use('/api/map', mapRoutes);
 app.use('/api/province', provinceRoutes);
@@ -52,8 +43,8 @@ app.use((req, res, next) => {
 });
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT} in ${config.NODE_ENV} mode`);
 });
 
 export default app;
