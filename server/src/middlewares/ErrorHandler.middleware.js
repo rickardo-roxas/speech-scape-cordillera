03499960cded0e308390abf7401b7320ea5efabc
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-17 21:53:26 +0800

    Added response handler in error handler middleware

--- server/src/middlewares/ErrorHandler.middleware.js
+++ server/src/middlewares/ErrorHandler.middleware.js
@@ -1,3 +1,5 @@
+import { errorResponse } from '../utils/ResponseHandler.util.js';
+
 // This middleware handles errors for the whole server.
 function errorHandler(err, _req, res, _next) {
     console.error(`[ERROR] ${err.name || 'Error'}: ${err.message}`);
@@ -5,9 +7,9 @@ function errorHandler(err, _req, res, _next) {
 
     const status = err.statusCode || 500;
 
-    res.status(status).json({
-        success: false,
-        message: err.message || 'Internal Server Error',
+    return errorResponse(res, {
+        message: err.message,
+        status: status,
     });
 }
 
