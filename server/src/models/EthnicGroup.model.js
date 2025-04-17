6c0b47c28ff983738083a7cffcd8484cb7621427
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-17 16:48:58 +0800

    Changed name param for conistency

--- server/src/models/EthnicGroup.model.js
+++ server/src/models/EthnicGroup.model.js
@@ -4,11 +4,11 @@
 class EthnicGroup {
     /**
      * Creates a new EthnicGroup object.
-     * @param {String} eg_name - Name of the ethnicity
+     * @param {String} ethnic_group_name - Name of the ethnicity
      * @param {float} percentage - Percentage of population
      */
-    constructor(eg_name, percentage) {
-        this.eg_name = eg_name;
+    constructor(ethnic_group_name, percentage) {
+        this.ethnic_group_name = ethnic_group_name;
         this.percentage = percentage;
     }
 }
