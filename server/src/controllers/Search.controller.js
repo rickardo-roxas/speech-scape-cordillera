17a55a5d13d9fdddba8d687f078503d6b0a81062
Sebastian Siccuan <2233205@slu.edu.ph>
2025-04-18 12:54:50 +0800

    fixed revision (rebased and pulled)

--- server/src/controllers/Search.controller.js
+++ server/src/controllers/Search.controller.js
@@ -1,21 +1,19 @@
-import { searchProvincesByName as searchProvincesFromModel } from '../models/Province.model.js';
-/**
- * Controller: Search provinces by name
- * Route: GET /api/search/provinces?keyword=abra
- */
-export async function searchProvincesByName(req, res, next) {
-    try {
-        const keyword = req.query.keyword;
+import ProvinceModel from '../models/Province.model.js';
 
-        if (!keyword) {
-            return res.status(400).json({ error: "Keyword query param is required" });
+const searchProvinceByName = async (req, res, next) => {
+    try {
+        const name = req.params.name;
+
+        if (!name) {
+            return res.status(400).json({ error: "Province name is required" });
         }
 
-        const results = await searchProvincesFromModel(keyword);
-
+        const results = await ProvinceModel.searchProvinceByName(name);
         return res.status(200).json(results);
     } catch (err) {
         console.error("Search controller error:", err);
         next(err);
     }
-}
+};
+
+export { searchProvinceByName };
--- server/src/routes/Search.routes.js
+++ server/src/routes/Search.routes.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { searchProvincesByName } from '../controllers/Search.controller.js';
+import { searchProvinceByName } from '../controllers/Search.controller.js';
 
 const router = express.Router();
-router.get('/', (req,res) => {
 
-router.get('/provinces', searchProvincesByName);
+router.get('/provinces/:name', searchProvinceByName);
 
 export default router;
