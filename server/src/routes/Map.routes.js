784c9c2ca8a9132ce15303650359ebfba2bd3f21
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-18 10:01:27 +0800

    Removed unused imports and variables

--- server/src/routes/Map.routes.js
+++ server/src/routes/Map.routes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import MapController from '../controllers/Map.controller.js';
-import { successResponse, errorResponse } from '../utils/ResponseHandler.util.js';
+import { successResponse } from '../utils/ResponseHandler.util.js';
 
 const router = express.Router();
 
 // Default map of the Philippines
 router.get('/' , (_req,res) => {
     return successResponse(res, {
-        message: "Map ready. Select a province or city to get started.",
+        message: "Select a province or city to get started.",
     });
 });
 
 // Display a specific province based on name
-router.get('/:name', (req, res) => {
+router.get('/:name', (req, _res) => {
     const { type } = req.query;
 
     if (type === 'Province') {
