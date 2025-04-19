8151189fb70b0f3fe295e181a47d7e8a690c0632
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-20 07:31:15 +0800

    Added initial API fetching

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-// import useFetch from '../hooks/UseFetch.hook';
+import React, { useEffect, useState } from 'react';
+import useFetch from '../hooks/UseFetch.hook';
 
 import Card from '../components/layout/Cards/Card';
 import TextContainer from '../components/layout/TextContainer/TextContainer';
-import Badge from '../components/ui/Badge/Badge';
 import PhilippineMap from '../components/features/map/PhilippineMap';
 
 import cordilleraGeoJSON from '../assets/json/cordillera.json';
@@ -14,8 +13,38 @@ import styles from './MapPage.module.css';
  * @returns {JSX.Element} 
  */
 function MapPage() {
-    //const [selectedProvince, setSelectedProvince] = useState(null);
-    //const [provinceData, setProvinceData] = useState(null);
+    // Location features from map
+    const [selectedLocationName, setSelectedLocationName] = useState('');
+    const [selectedLocationType, setSelectedLocationType] = useState('');
+
+    // Location data
+    const [locationDetails, setLocationDetails] = useState(null);
+
+    const { data, loading, error, refetch } = useFetch(`/map/${selectedLocationType.toLowerCase()}/${selectedLocationName}`, {
+        method: "GET",
+    }, false);
+
+    useEffect(() => {
+        if (data && data.success && data.data) {
+            const details = data.data;
+            setLocationDetails({
+                name: details.name,
+                info_1: details.info_1,
+                info_2: details.info_2,
+                info_3: details.info_3,
+                sublocations: selectedLocationType === "Province" ? details.municipalities : details.barangays,
+                ethnicGroups: details.ethnic_groups,
+                languages: details.languages,
+                images: details.images,
+            });
+        }
+    }, [data]);
+
+    useEffect(() => {
+        if (selectedLocationName && selectedLocationType) {
+            refetch();
+        }
+    }, [selectedLocationName, selectedLocationType]);    
 
     /**
      * Function to handle events for each GeoJSON feature upon user click.
@@ -25,34 +54,41 @@ function MapPage() {
     const onEachFeature = (feature, layer) => {
         layer.bindPopup(feature.properties.name);
         layer.on('click', () => {
-            setSelectedProvince(feature.properties.name);
+            setSelectedLocationName(feature.properties.name);
+            setSelectedLocationType(feature.properties.type);
         });
-    }
+    };
 
     return (
         <div className={styles.div}>
             <Card
-                header="Province"
+                header={locationDetails && locationDetails.name ? locationDetails.name : 'Information'}
                 className={styles.profile}
             >
-                <TextContainer 
-                    title="Heading"
-                >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    <Badge>
-                        Province
-                    </Badge>
+                
+                <TextContainer title="Overview">
+                    {locationDetails ? (
+                        <>
+                        <p><strong>Name:</strong> {locationDetails.name}</p>
+                        </>
+                    ) : (
+                        <em>Click a province or city to load data.</em>
+                    )}
                 </TextContainer>
-                <TextContainer 
-                    title="Heading"
-                >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </TextContainer>
+
+                {locationDetails && locationDetails.ethnicGroups && (
+                    <TextContainer title="Ethnic Groups">
+                        {/* Render ethnic groups here */}
+
+                    </TextContainer>
+                )}
+
+                {locationDetails && locationDetails.languages && (
+                    <TextContainer title="Languages">
+                        {/* Render languages here */}
+                        
+                    </TextContainer>
+                )}
             </Card>
 
             <Card
