72ad460c2d2f26cfc2e6e0b30c805f73e00420cf
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-19 11:11:25 +0800

    Fixed routing of landing page

--- server/src/index.js
+++ server/src/index.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, "../public",)));
 app.use(logger);
 
 // Routes
-app.get("/api", landingRoutes);
+app.use("/api", landingRoutes);
 app.use('/api/map', mapRoutes);
 app.use('/api/province-city', provinceRoutes);
 app.use('/api/search', searchRoutes);
