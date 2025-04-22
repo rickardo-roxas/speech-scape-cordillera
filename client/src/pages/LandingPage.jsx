8e6dc41c84d220d35a57bff6abd4cd04eafcc16d
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-04-22 11:54:04 +0800

    Fixed region overview

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -7,7 +7,7 @@ import LinkButton from '../components/ui/Button/LinkButton';
 import Carousel from '../components/ui/Carousel/Carousel';
 import PHMapIcon from '../components/ui/PHMapIcon';
 
-import cordilleranWeave from '../assets/images/weave-cordillera.png';
+import cordilleranWeave from '../assets/images/cordilleran-weave.png';
 import styles from './LandingPage.module.css';
 
 /**
@@ -43,7 +43,6 @@ function LandingPage() {
         <>
             {/* Static Components */}
             <div className={styles.hero}>
-
                 <TextContainer
                     className={styles.headline}
                     contentClassName={styles.headlineText}
@@ -57,11 +56,13 @@ function LandingPage() {
                         <li>Accurate distribution of languages and ethnic groups</li>
                         <li>Galleries of famous landmarks</li>
                     </ul>
-                    <LinkButton
-                        label="Learn More"
-                        to='#cordillera'
-                        className={styles.cta}
-                    />
+                    <div className={styles.ctaContainer}>
+                        <LinkButton
+                            label="Learn More"
+                            to='#cordillera'
+                            className={styles.cta}
+                        />
+                    </div> 
                 </TextContainer>
 
                 <PHMapIcon className={styles.heroImage} />
@@ -70,93 +71,49 @@ function LandingPage() {
 
             {/* Dynamic Regional Components */}
             <div className={styles.regions}>
-
+                <span>
+                    <Image src={cordilleranWeave} />
+                </span>
                 {cordilleraData && (
                     <div className={styles.cordillera} id='cordillera'>
+                        
+                        <TextContainer contentClassName={styles.regionsContainer}>
+                            <div className={styles.regionsOverview}>
+                                {/* Overview */}
+                                <h2>{cordilleraData.name}</h2>
 
-                        <TextContainer title={cordilleraData.name}>
-                            
-                            {/* Overview */}
-                            <TextContainer
-                                title='Overview'
-                                className={styles.regionsText}
-                            >
-                                <ul>
-                                    <li>
-                                        {cordilleraData.info_1}
-                                    </li>
-                                    <li>
-                                        {cordilleraData.info_2}
-                                    </li>
-                                    <li>
-                                        {cordilleraData.info_3}
-                                    </li>
-                                </ul>
+                                <p>{cordilleraData.info_1}</p>
+                                <p>{cordilleraData.info_2}</p>
+                                <p>{cordilleraData.info_3}</p>
 
-                                {/* Cities */}
-                                <TextContainer
-                                    title='Cities'
-                                    className={styles.regionsOverview}
-                                >
-                                    <ul>
-                                        {cordilleraData.cities.map((city, index) => (
-                                            <li key={index}>
-                                                {city.name}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </TextContainer>
+                                {/* CTA Links */}
+                                <div className={styles.linkButtons}>
+                                    <LinkButton 
+                                        label="View Provinces and Cities"
+                                        to='/province-city/'
+                                        className='locationsButton'
+                                    />
 
-                                {/* Provinces */}
-                                <TextContainer
-                                    title='Provinces'
-                                    className={styles.regionsOverview}
-                                >
-                                    <ul>
-                                        {cordilleraData.provinces.map((province, index) => (
-                                            <li key={index}>
-                                                {province.name}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </TextContainer>
-                            </TextContainer>
+                                    <LinkButton 
+                                        label="Explore Map"
+                                        to='/map'
+                                        className={styles.mapButton}
+                                    />
+                                </div>
+                            </div>
 
-                            {/* CTA Links */}
-                            <TextContainer className='linkButtons'>
-
-                                <LinkButton 
-                                    label="View Provinces and Cities"
-                                    to='/province-city/'
-                                    className=''
-                                />
-
-                                <LinkButton 
-                                    label="Explore Map"
-                                    to='/map'
-                                    className=''
-                                />
-
-                            </TextContainer>
-
-                        </TextContainer>
-
-                        {/* Image Carousels */}
-                        <TextContainer>
-
+                            {/* Images Carousel */}
                             <Carousel>
                                 
-                                {[
-                                    ...(cordilleraData.provinces?.flatMap(p => p.images.map(img => `/images${img}`)) || []),
-                                    ...(cordilleraData.cities?.flatMap(c => Object.values(c.images).map(img => `/images${img}`)) || [])
-                                ].map((imgSrc, index) => (
-                                    <Image key={index} src={imgSrc} />
-                                ))}
+                                    {[
+                                        ...(cordilleraData.provinces?.flatMap(p => p.images.map(img => `/images${img}`)) || []),
+                                        ...(cordilleraData.cities?.flatMap(c => Object.values(c.images).map(img => `/images${img}`)) || [])
+                                    ].map((imgSrc, index) => (
+                                        <Image key={index} src={imgSrc} />
+                                    ))}
 
                             </Carousel>
-
                         </TextContainer>
-
                     </div>
                 )}
             </div>
