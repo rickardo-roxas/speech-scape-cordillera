aa4db93566a9f3563f20c27672f3c49227a6eb57
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2025-04-23 01:21:13 +0800

    Added languages component

---
+++ client/src/components/features/ProvinceCityProfile/Languages.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import SectionTitle from './SectionTitle';
+import styles from './Languages.module.css';
+
+function Languages({ languages }) {
+    return (
+        <div className={styles.languages}>
+            <SectionTitle title="Languages" />
+            {languages.map((language, index) => (
+                <div key={index}>
+                    <ProgressBar className={styles.customProgress}>
+                        <ProgressBar 
+                            now={Number(language.percentage) + 16} 
+                            variant="success" 
+                            label={`${language.name}`} 
+                        />
+                        <ProgressBar 
+                            now={100 - Number(language.percentage)} 
+                            label={`${language.percentage}%`} 
+                        />
+                    </ProgressBar>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Languages;
