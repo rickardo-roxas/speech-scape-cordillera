86ca38c807812f9f60ffdb5d8a57a2e1680e7920
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2025-04-23 11:12:09 +0800

    Refactored ProvincePage and ProvincesAndCitiesPage

--- client/src/pages/ProvincePage.jsx
+++ client/src/pages/ProvincePage.jsx
@@ -1,8 +1,9 @@
 // Libraries and dependencies
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useFetch from '../hooks/UseFetch.hook';
 
 // Components
-// import other components here.
+import BannerContainer from '../components/features/ProvinceCityProfile/BannerContainer';
 // import styles from './ProvincePage.module.css';
 
 /**
@@ -10,8 +11,37 @@ import React from 'react';
  * @returns {JSX.Element}
  */
 function ProvincePage() {
+    const [selectedProvince] = useState('');
+    const [provinceData, setProvinceData] = useState(null);
+
+    const { data, refetch } = useFetch(`/map/province/${selectedProvince}`, {
+        method: 'GET',
+    }, false);
+
+    useEffect(() => {
+        if (selectedProvince) {
+            refetch();
+        }
+    }, [selectedProvince, refetch]);
+
+    useEffect(() => {
+        if (data && data.success && data.data) {
+            setProvinceData(data.data);
+        }
+    }, [data]);
+
     return (
-        <div>Province Page</div>
+        <div>
+            <div>ProvinceAndCities</div>
+            {provinceData && (
+                <BannerContainer
+                    backgroundImage={provinceData.banner_image || 'placeholder.jpg'}
+                    title={provinceData.name}
+                    description={provinceData.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+                    type="Province"
+                />
+            )}
+        </div>
     );
 }
 
--- client/src/pages/ProvincesAndCitiesPage.jsx
+++ client/src/pages/ProvincesAndCitiesPage.jsx
@@ -1,9 +1,9 @@
 // Libraries and dependencies
-import React, { useState, useEffect } from 'react';
-import useFetch from '../hooks/UseFetch.hook';
+import React from 'react';
+// import useFetch from '../hooks/UseFetch.hook';
 
 // Components
-import BannerContainer from '../components/features/ProvinceCityProfile/BannerContainer';
+// import other components here.
 // import styles from './ProvincesAndCitiesPage.module.css';
 
 /**
@@ -11,37 +11,8 @@ import BannerContainer from '../components/features/ProvinceCityProfile/BannerCo
  * @returns {JSX.Element}
  */
 function ProvincesAndCitiesPage() {
-    const [selectedProvince] = useState('');
-    const [provinceData, setProvinceData] = useState(null);
-
-    const { data, refetch } = useFetch(`/map/province/${selectedProvince}`, {
-        method: 'GET',
-    }, false);
-
-    useEffect(() => {
-        if (selectedProvince) {
-            refetch();
-        }
-    }, [selectedProvince, refetch]);
-
-    useEffect(() => {
-        if (data && data.success && data.data) {
-            setProvinceData(data.data);
-        }
-    }, [data]);
-
     return (
-        <div>
-            <div>ProvinceAndCities</div>
-            {provinceData && (
-                <BannerContainer
-                    backgroundImage={provinceData.banner_image || 'placeholder.jpg'}
-                    title={provinceData.name}
-                    description={provinceData.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-                    type="Province"
-                />
-            )}
-        </div>
+        <div>ProvinceAndCities</div>
     );
 }
 
