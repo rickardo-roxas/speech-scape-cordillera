ab802232cfbcce5e263673e566e60a9158cbb322
JOHN REY DE TORRES <118342555+JohnReyDT@users.noreply.github.com>
2025-04-24 23:51:23 +0800

    Created ProvinceName component for province titles and update styles in RegionName and ProvinceIntro

--- client/src/components/features/ProvinceCities/Components/ProvinceName.jsx
+++ client/src/components/features/ProvinceCities/Components/ProvinceName.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import styles from './Province.module.css';
+import TypeTag from '../../../ui/Badge/Badge';
 
 function ProvinceName({ name = "Province Name" }) {
     return (
       <div className={styles.container}> 
         <h3 className={styles.header}>{name}</h3>
+      <TypeTag label="Province" />
       </div>
     );
   }
