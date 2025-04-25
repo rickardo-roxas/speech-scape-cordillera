aedfec6431ca7ca649e87cd4ed388eda58ffeb6c
JOHN REY DE TORRES <118342555+JohnReyDT@users.noreply.github.com>
2025-04-25 19:13:49 +0800

    docs: Add JSDoc comments to ProvincesAndCitiesPage and related containers; remove unnecessary JSX files

--- client/src/components/features/ProvinceCities/CARContainer/ImageContainer.jsx
+++ client/src/components/features/ProvinceCities/CARContainer/ImageContainer.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import styles from './ProvinceImage.module.css';
 
+/**
+ * Renders an image within a styled container, allowing customization of size and additional styles.
+ *
+ * @component
+ * @param {Object} props - Component props
+ * @param {string} props.src - The image source URL
+ * @param {string} [props.alt=""] - Alternative text for the image (used for accessibility)
+ * @param {string} [props.width="100%"] - Width of the image container
+ * @param {string} [props.height="200px"] - Height of the image container (default is 200px)
+ * @param {string} [props.className=""] - Additional class names for styling the container
+ * @returns {JSX.Element} A responsive image wrapped in a styled container
+ */
 
 function ImageContainer({
   src,
